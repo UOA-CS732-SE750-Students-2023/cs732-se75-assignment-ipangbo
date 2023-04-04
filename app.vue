1217ae1fa13e92a1b7e6772538c18a03bdd555e4
Bo Pang <i@ipangbo.cn>
2023-04-05 00:32:55 +1200

    🔧 Register Element icons in Vue app

--- app.vue
+++ app.vue
@@ -7,8 +7,16 @@
 </template>
 
 <script setup lang="ts">
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "~/assets/css/base.css";
 import "~/assets/css/main.css";
+
+const nuxtApp = useNuxtApp()
+// console.log(nuxtApp)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  nuxtApp.vueApp.component(key, component)
+}
+
 
 const layout = "default"
 const blogTitle = useRuntimeConfig().public.blogTitle;
